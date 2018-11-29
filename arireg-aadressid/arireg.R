545e62046aad12dec86470833631d5eaee0eb5ee
Annegrete Peek <annegretepeek@gmail.com>
2018-11-29 15:14:47 +0200

    added data

--- arireg-aadressid/arireg.R
+++ arireg-aadressid/arireg.R
@@ -5,7 +5,7 @@ library(stringr)
 library(readxl)
 
 #Äriregister seisuga 21.11.18
-arireg <- fread("C:/Users/annegrete.peek/Desktop/Arireg/ettevotja_rekvisiidid_2018-11-21.csv", encoding = "UTF-8")
+arireg <- fread("ettevotja_rekvisiidid_2018-11-21.csv", encoding = "UTF-8")
 arireg <- arireg %>% 
   mutate(viimane = word(ads_normaliseeritud_taisaadress, -1),
          maja = ifelse(grepl("[[:digit:]]", viimane), viimane, NA),
@@ -16,13 +16,13 @@ arireg <- arireg %>%
                             ads_normaliseeritud_taisaadress))
 
 #Maksuvõlg seisuga 02.11.18
-volg <- read_excel("C:/Users/annegrete.peek/Desktop/Arireg/maksuvolglaste_nimekiri.xlsx", skip = 2)
+volg <- read_excel("maksuvolglaste_nimekiri.xlsx", skip = 2)
 arireg <- arireg %>% mutate(volg = ifelse(ariregistri_kood %in% volg$Kood, 1, 0))
 
 aadress <- arireg %>% group_by(korterita) %>% summarise(arv = n(), volg = sum(volg)) %>% ungroup() %>% arrange(-arv)
 
 #Ehitised seisuga 24.11.18
-ehitis <- fread("C:/Users/annegrete.peek/Desktop/Arireg/eh_ehitised_1994-01_2018-11-24.csv", encoding = "UTF-8") %>% filter(rajatis_hoone == "H") %>% 
+ehitis <- fread("eh_ehitised_1994-01_2018-11-24.csv", encoding = "UTF-8") %>% filter(rajatis_hoone == "H") %>% 
   select(esmane_kasutus, nimetus, maakond, omavalitsus, taisaadress, lahiaadress, ehitisalune_pind, kasulik_pind, maht_bruto, suletud_netopind) 
 ehitis2 <- ehitis %>% filter(taisaadress %in% arireg$korterita & taisaadress != "") %>% arrange(taisaadress, -ehitisalune_pind) %>% 
   group_by(taisaadress) %>% slice(1)
